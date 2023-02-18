9acdbe506507a79480674fe30ad4799daa1963d6
Mayuresh <mayureshpatil068@gmail.com>
2023-02-18 17:36:38 +0530

    EmailsAssignment

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,34 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+
+        if(oldPassword.equals(password)){
+            if(isValid(newPassword)){
+                System.out.println("password change successfully");
+            }
+            else{
+                System.out.println("password not matching old password");
+            }
+        }
+    }
+    public boolean isValid(String newPassword){
+        boolean uppercase=false;
+        boolean lowercase=false;
+        boolean digit=false;
+        boolean specialchar=false;
+        if(newPassword.length()<8)return false;
+
+        for(int i=0;i<newPassword.length();i++){
+            char ch=newPassword.charAt(i);
+            if(ch>='a' && ch<='z')lowercase=true;
+            else if(ch>='A' && ch<='z')uppercase=true;
+            else if(ch>=0 && ch<=9)digit=true;
+            else specialchar=true;
+        }
+
+        if(lowercase && uppercase && digit && specialchar){
+            return true;
+        }
+        return false;
     }
 }
